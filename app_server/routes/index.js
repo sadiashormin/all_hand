33575f6db247301bb5572a8596df120466195e2c
mrrahman <mohammad.rahman@viasat.com>
2017-06-08 14:52:25 -0500

    location list

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain=require("../controllers/main");
-/* GET home page. */
-router.get('/', ctrlMain.index);
+
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
+
+
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
